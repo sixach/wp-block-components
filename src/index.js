59e322e61ab6f0017bbb795aa48f0c2e11b16894
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-05-31 16:04:09 +0300

    Exported components

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export { default as BackgroundSizeControl } from './background-size-control';
+export { default as ConditionalWrap } from './conditional-wrap';
 export { default as GenerateSvgPaths } from './generate-svg-paths';
 export { default as InnerHTML } from './inner-html';
 export { default as Loading } from './loading';
+export { default as PrintHTML } from './print-html';
