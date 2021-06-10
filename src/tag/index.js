efecb45b915f18a4294650297c41b6f51ac30a0b
Markus <markus.goeckeritz@sixa.ch>
2021-06-10 11:58:08 +0200

    renamed id and classname

--- src/tag/index.js
+++ src/tag/index.js
@@ -56,7 +56,7 @@ import { decodeEntities } from '@wordpress/html-entities';
  * />
  */
 const Tag = ( { instanceId, label, onRemove, className } ) => (
-	<span id={ `components-tag-${ instanceId }` } className={ classnames( 'components-tag', className ) }>
+	<span id={ `sixa-component-tag-${ instanceId }` } className={ classnames( 'sixa-component-tag', className ) }>
 		{ escapeHTML( decodeEntities( label ) ) }
 		<Button variant="link" icon="remove" isSmall onClick={ onRemove } />
 	</span>
