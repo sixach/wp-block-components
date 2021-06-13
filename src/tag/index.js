7107e6e58208fbc0919f387b3de4c71dc1a28c67
Markus <markus.goeckeritz@sixa.ch>
2021-06-13 18:28:21 +0200

    added VisuallyHidden, screenReaderText and button label

--- src/tag/index.js
+++ src/tag/index.js
@@ -12,7 +12,7 @@ import classnames from 'classnames';
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
  * @ignore
  */
-import { Button } from '@wordpress/components';
+import { Button, VisuallyHidden } from '@wordpress/components';
 
 /**
  * Collection of handy hooks and higher-order components (HOCs) to wrap WordPress
@@ -40,29 +40,40 @@ import { escapeHTML } from '@wordpress/escape-html';
 import { decodeEntities } from '@wordpress/html-entities';
 
 /**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
+ * @ignore
+ */
+import { __, sprintf } from '@wordpress/i18n';
+
+/**
  * Tag element with a remove button.
  * This component is typically used as the output when iterating over a collection of items that should
  * be displayed as tags rather than standalone.
  *
  * @function
  * @since		1.1.0
- * @param		{Object}		props 					The props that were defined by the caller of this component.
- * @param		{string}		props.label 			Label shown in the element.
- * @param		{number}		props.instanceId		Unique ID of the component.
- * @param		{Function}		props.onRemove 			Callback function to trigger when the remove button is clicked.
- * @param		{string}		props.className 		The class that will be added to the classes of the wrapper span.
- * @return		{JSX.Element} 							Tag element.
+ * @param		{Object}		props 						The props that were defined by the caller of this component.
+ * @param		{string}		props.label 				Label shown in the element.
+ * @param		{number}		props.instanceId			Unique ID of the component.
+ * @param		{Function}		props.onRemove 				Callback function to trigger when the remove button is clicked.
+ * @param		{string}		props.screenReaderText		Text to be used for screen readers.
+ * @param		{string}		props.className 			The class that will be added to the classes of the wrapper span.
+ * @return		{JSX.Element} 								Tag element.
  * @example
  *
  * <Tag
  * 		label={ label }
  * 		onRemove={ handleOnClickTag }
+ * 		screenReaderText={ __( ')}
  * />
  */
-const Tag = ( { instanceId, label, onRemove, className } ) => (
+const Tag = ( { instanceId, label, onRemove, className, screenReaderText } ) => (
 	<span id={ `sixa-component-tag-${ instanceId }` } className={ classnames( 'sixa-component-tag', className ) }>
-		{ escapeHTML( decodeEntities( label ) ) }
-		{ !! onRemove && <Button className="sixa-component-tag__remove-button" variant="link" icon="remove" isSmall onClick={ onRemove } /> }
+		<VisuallyHidden as="span">{ screenReaderText || label }</VisuallyHidden>
+		<span aria-hidden="true">{ escapeHTML( decodeEntities( label ) ) }</span>
+		{ !! onRemove && <Button className="sixa-component-tag__remove-button" variant="link" icon="remove" isSmall onClick={ onRemove } label={ sprintf( __('Remove %s', 'sixa' ), label )} /> }
 	</span>
 );
 
