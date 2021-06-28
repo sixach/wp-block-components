b1b2fd9ece88cb65bc404ac573e571130a87f6b4
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-06-28 20:19:35 +0300

    Allow tags to be sorted via drag & drop

--- src/multi-select/selected-tag-list.js
+++ src/multi-select/selected-tag-list.js
@@ -3,7 +3,22 @@
  *
  * @ignore
  */
-import { map } from 'lodash';
+import { map, nth, forEach } from 'lodash';
+
+/**
+ * Runtime type checking for React props and similar objects.
+ *
+ * @ignore
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * WordPress specific abstraction layer atop React.
+ *
+ * @see		https://github.com/WordPress/gutenberg/tree/HEAD/packages/element/README.md
+ * @ignore
+ */
+import { useState, useEffect, useMemo, useCallback } from '@wordpress/element';
 
 /**
  * The styled Tag component generated using @emotion/react API.
@@ -27,26 +42,54 @@ import { SelectedTagListWrapper } from './style';
  * provided as { label, value } pairs and can be deselected by clicking the remove button.
  *
  * @function
- * @since		1.1.0
+ * @since		1.2.0
  * @param		{Object}		props				The props that were defined by the caller of this component.
  * @param		{Array}			props.items			List of items that should be displayed as Tags.
+ * @param		{Function}		props.onChange		Callback function to be triggered when the user finishes a sorting gesture.
  * @param		{Function}		props.onRemove		Callback function to trigger when the remove button in a tag is clicked.
  * @return		{JSX.Element}						Horizontal list of Tags.
  * @example
  *
  * <SelectedTagList
  * 		items={ selectedOptions}
+ * 		onChange={ onChange }
  * 		onRemove={ handleOnClickSelectedOptionTag }
  * 	/>
  */
-function SelectedTagList( { items, onRemove } ) {
-	return (
-		<SelectedTagListWrapper className="sixa-component-multiselect__tag-list">
-			{ map( items, ( { label }, index ) => (
-				<StyledTag key={ index } label={ label } onRemove={ () => onRemove( index ) } />
-			) ) }
-		</SelectedTagListWrapper>
-	);
+function SelectedTagList( { items, onChange, onRemove } ) {
+	const [ options, setOptions ] = useState( [] );
+	const { values, labels } = useMemo( () => {
+		const tempValues = [];
+		const tempLabels = [];
+
+		forEach( items, ( { value, label } ) => {
+			tempValues.push( value );
+			tempLabels.push( label );
+		} );
+		return {
+			values: tempValues,
+			labels: tempLabels,
+		};
+	}, [ items ] );
+	const handleOnChange = useCallback( ( newItems ) => {
+		onChange( map( newItems, ( { props: { value } } ) => value ) );
+	}, [] );
+
+	useEffect( () => {
+		setOptions(
+			map( items, ( i, index ) => (
+				<StyledTag key={ index } label={ nth( labels, index ) } value={ nth( values, index ) } onRemove={ () => onRemove( index ) } />
+			) )
+		);
+	}, [ items ] );
+
+	return <SelectedTagListWrapper className="sixa-component-multiselect__tag-list" items={ options } onChange={ handleOnChange } />;
 }
+
+SelectedTagList.propTypes = {
+	items: PropTypes.array.isRequired,
+	onChange: PropTypes.func.isRequired,
+	onRemove: PropTypes.func.isRequired,
+};
 
 export default SelectedTagList;
