05c992b6687b7e6e876c967165f7fdde194a11bc
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-07-07 01:00:01 +0300

    Wrap a story in a few extra functionality

---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+/**
+ * External dependencies
+ */
+import { addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+import { withKnobs } from '@storybook/addon-knobs';
+
+addDecorator( withA11y );
+addDecorator( withKnobs );
