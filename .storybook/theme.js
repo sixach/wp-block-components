fc7bd3dec255e282b186acfb926819e1c1b5f26c
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-07-07 00:56:12 +0300

    Created a theme

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+/**
+ * External dependencies
+ */
+import { create } from '@storybook/theming';
+
+export default create( {
+	base: 'light',
+	brandTitle: 'Sixa UI Components',
+	brandImage: 'https://sixa.ch/wp-content/uploads/2021/03/logo.svg',
+} );
