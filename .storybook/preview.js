22af0777ee1e9aa5c62d2d742318b4fe84b94c9e
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-07-08 00:12:49 +0300

    Imported `@wordpress/components` stylesheet

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,5 +5,10 @@ import { addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 
+/**
+ * WordPress dependencies
+ */
+import '@wordpress/components/build-style/style.css';
+
 addDecorator( withA11y );
 addDecorator( withKnobs );
