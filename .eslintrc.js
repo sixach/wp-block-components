4922a02cb79d037d107ce19272e659a2f90461cc
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-07-08 00:10:06 +0300

    `eslint` silence no longer needed

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-const eslintConfig = {
-	root: true,
-	extends: [ 'plugin:@wordpress/eslint-plugin/recommended' ],
-	rules: {
-		'import/no-unresolved': 'off',
-	},
-};
-
-module.exports = eslintConfig;
