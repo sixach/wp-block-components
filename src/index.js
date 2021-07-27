148d9e12294cfe430ead9486458c9c41b00dd8a6
Markus <markus.goeckeritz@sixa.ch>
2021-07-27 13:29:17 +0200

    exported new components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 export { default as BackgroundSizeControl } from './background-size-control';
+export { default as BlockAlignmentMatrixControl } from './block-alignment-matrix-control';
 export { default as ConditionalWrap } from './conditional-wrap';
+export { default as FullHeightAlignmentControl } from './full-height-alignment-control';
 export { default as GenerateSvgPaths } from './generate-svg-paths';
 export { default as InnerHTML } from './inner-html';
 export { default as Loading } from './loading';
