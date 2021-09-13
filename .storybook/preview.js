ac5f9e2660bb8329be3a19721b441916b5e6f574
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-09-13 23:11:26 +0300

    Removed decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,5 @@
 /**
- * External dependencies
- */
-import { addDecorator } from '@storybook/react';
-import { withA11y } from '@storybook/addon-a11y';
-import { withKnobs } from '@storybook/addon-knobs';
-
-/**
  * WordPress dependencies
  */
 import '@wordpress/components/build-style/style.css';
 import 'glider-js/glider.min.css';
-
-addDecorator( withA11y );
-addDecorator( withKnobs );
