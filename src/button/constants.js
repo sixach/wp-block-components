1dbb39d580dc1ff15d05396bc29087480013ea70
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-09-16 14:43:05 +0300

    Component: Button

---
+++ src/button/constants.js
@@ -0,0 +1,9 @@
+/**
+ * The object exported from this file has been determined to be frozen mainly
+ * to prevent new properties from being added to it, existing properties from
+ * being removed, and avoid the values of existing properties from being changed.
+ */
+export default Object.freeze( {
+	ALLOWED_FORMATS: [ 'core/bold', 'core/italic' ],
+	BUTTON_CLASSNAME: 'button wp-block-button__link',
+} );
