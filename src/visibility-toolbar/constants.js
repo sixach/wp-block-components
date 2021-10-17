4013b180ad40ef5ff6abcb0a2a88cee6b2bb89ae
Markus <markus.goeckeritz@sixa.ch>
2021-10-17 23:27:04 +0200

    fixed colors

--- src/visibility-toolbar/constants.js
+++ src/visibility-toolbar/constants.js
@@ -4,6 +4,6 @@
  * being removed, and avoid the values of existing properties from being changed.
  */
 export default Object.freeze( {
-	ACTIVE_COLORS: [ '#1E1E1E', '#FFF', '#9DD1FF' ],
-	COLORS: [ '#FDBCCB', '#FFF', '#1E1E1E' ],
+	ACTIVE_COLORS: [ '#FDBCCB', '#FFF', '#1E1E1E' ],
+	COLORS: [ '#9DD1FF', '#FFF', '#FFF' ],
 } );
