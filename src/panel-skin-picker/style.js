58b40d845d38dab397f3eb221c1c9bcde17e01db
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-10-31 20:55:24 +0300

    Exported styled components

---
+++ src/panel-skin-picker/style.js
@@ -0,0 +1,81 @@
+/**
+ * The styled API for @emotion/react.
+ *
+ * @see    https://www.npmjs.com/package/@emotion/styled
+ */
+import styled from '@emotion/styled';
+
+/**
+ * This packages includes a library of generic WordPress components to be used for
+ * creating common UI elements shared between screens and features of the WordPress dashboard.
+ *
+ * @see    https://developer.wordpress.org/block-editor/reference-guides/packages/packages-components/
+ */
+import { Button, Flex, FlexItem } from '@wordpress/components';
+
+/**
+ * Wrapper around the inner child components.
+ *
+ * @constant
+ * @since	    2.0.0
+ * @type		{JSX.Element}
+ */
+export const Wrapper = styled( Flex )`
+	gap: 10px;
+	height: 100%;
+	margin-bottom: 10px;
+	width: 100%;
+`;
+
+/**
+ * Skin item wrapper.
+ *
+ * @constant
+ * @since	    2.0.0
+ * @type		{JSX.Element}
+ */
+export const Item = styled( FlexItem )`
+	margin: 0 !important;
+	width: calc( 50% - 5px );
+`;
+
+/**
+ * Skin element.
+ *
+ * @constant
+ * @since	    2.0.0
+ * @type		{JSX.Element}
+ */
+export const Skin = styled( Button )`
+	align-items: center;
+	display: flex;
+	justify-content: center;
+	height: 80px;
+	opacity: ${ ( { isSelected } ) => ( isSelected ? 1 : 0.6 ) };
+	padding: 14px 0;
+	position: relative;
+	width: 100%;
+
+	span {
+		padding: 0;
+	}
+
+	&:before {
+		background-image: ${ ( { imageUri } ) => `url(${ imageUri })` };
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: 150% 140%;
+		content: '';
+		height: ${ ( { isSelected } ) => `calc(100% - ${ isSelected ? '6px' : '2px' })` };
+		position: absolute;
+		width: ${ ( { isSelected } ) => `calc(100% - ${ isSelected ? '6px' : '2px' })` };
+	}
+
+	&:hover {
+		opacity: 1;
+	}
+
+	&:focus {
+		box-shadow: none;
+	}
+`;
