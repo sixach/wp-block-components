bf2a50d250dfc452cb01531b40b358c7bf0ec0ec
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-12-03 21:56:13 +0300

    Exported component script

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ export { default as MediaUploadToolbar } from './media-upload-toolbar';
 export { default as MultiSelect } from './multi-select';
 export { default as NodeList } from './node-list';
 export { default as NumberToolbar } from './number-toolbar';
+export { default as PanelColumnSettings } from './panel-column-settings';
 export { default as PanelThemePicker } from './panel-theme-picker';
 export { default as PanelViewports } from './panel-viewports';
 export { default as PostSelectForm } from './post-select-form';
