b8ab70e067b5bbc3841d89bae76f08192454f6c7
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-12-13 13:37:59 +0300

    Default border radius edge values

---
+++ src/border-radius-control/constants.js
@@ -0,0 +1,13 @@
+/**
+ * The object exported from this file has been determined to be frozen mainly
+ * to prevent new properties from being added to it, existing properties from
+ * being removed, and avoid the values of existing properties from being changed.
+ */
+export default Object.freeze( {
+	DEFAULT_VALUES: {
+		topLeft: null,
+		topRight: null,
+		bottomLeft: null,
+		bottomRight: null,
+	},
+} );
