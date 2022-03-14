26c50c36b509e097c2c4aa6ab3fe82184bc7a87d
Adrian Firoiu <mihai.a.firoiu@gmail.com>
2022-03-14 18:02:45 +0100

    Import Flickity styles and update component

--- src/slider/index.js
+++ src/slider/index.js
@@ -33,11 +33,11 @@ import Flickity from 'react-flickity-component';
 import defaultOptions from './options';
 
 /**
- * The styled components generated using @emotion/react API.
+ * The Flickity default stylesheet.
  *
  * @see    https://www.npmjs.com/package/@emotion/styled
  */
-// import { Track } from './style';
+import 'flickity/dist/flickity.min.css';
 
 /**
  * A react wrapper for the react-glider package.
@@ -48,7 +48,6 @@ import defaultOptions from './options';
  * @param	   {Object}		  	props              The props that were defined by the caller of this component.
  * @param      {string}         props.className    The CSS class name(s) that will be added to the wrapper element.
  * @param  	   {JSX.Element}    props.children     Any React element or elements can be passed as children. They will be rendered within the wrapper.
- * @param      {string}         props.gap    	   The gap CSS property sets the gaps (gutters) between rows and columns.
  * @param  	   {Object} 		ref      		   Component enhancer used to enable passing a ref to its wrapped component.
  * @return	   {JSX.Element}    				   Slider component.
  * @example
@@ -60,13 +59,20 @@ import defaultOptions from './options';
  *	   <img src="https://picsum.photos/700/300" />
  * </Slider>
  */
-function Slider( { className, children, gap, ...wrapperProps }, ref ) {
+function Slider( { className, children, ...wrapperProps }, ref ) {
 	return (
-		<div className={ classnames( 'sixa-component-slider', 'flickity-wrap', className ) }>
-			<Flickity options={ { ...merge( {}, defaultOptions, wrapperProps ) } } reloadOnUpdate ref={ ref }>
-				{ children }
-			</Flickity>
-		</div>
+		<Flickity
+			className={ classnames(
+				'sixa-component-slider',
+				'flickity-wrap',
+				className
+			) }
+			options={ { ...merge( {}, defaultOptions, wrapperProps ) } }
+			reloadOnUpdate
+			ref={ ref }
+		>
+			{ children }
+		</Flickity>
 	);
 }
 
@@ -79,16 +85,11 @@ Slider.propTypes = {
 	 * React elements that are being passed as children.
 	 */
 	children: PropTypes.arrayOf( PropTypes.element ).isRequired,
-	/**
-	 * Extra spacing value between rows and columns of each slide.
-	 */
-	gap: PropTypes.string,
 };
 
 Slider.defaultProps = {
 	className: undefined,
 	children: undefined,
-	gap: '0',
 };
 
 export default forwardRef( Slider );
